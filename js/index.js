2dc933feebd416b3d8b1aa5ef25135022b69d893
Geeky-bit256 <mohapiwalter716@gmail.com>
2026-08-11 17:26:55 +0200

    Make header mobile responsive across all pages

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#navMenu");
+
+hamburger.addEventListener("click", () => {
+  
+  navMenu.classList.toggle("active");
+});
